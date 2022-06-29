034c3d561f6194440ea0c4eb900dc6b751a0ff19
Mariano Dominguez <>
2022-06-29 01:31:18 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+// function to calculate time countdown string
+function formatDate(date) {
+  var delta = Math.abs(new Date() - date);
+  var days = Math.floor(delta / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((delta % (1000 * 60)) / 1000);
+  console.log(days, hours, minutes, seconds);
+  return [
+    days > 0 ? days + " dias " : "",
+    hours > 0 ? hours + " horas " : "",
+    minutes > 0 ? minutes + " minutos " : "",
+    seconds > 0 ? seconds + " segundos " : "",
+  ].join("");
+}
+const dateInFuture = new Date("2022-10-01");
 
 export default function Home() {
+  const [time, setTime] = React.useState(formatDate(dateInFuture));
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(formatDate(dateInFuture));
+    }, 1000);
+  }, [time]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,57 +39,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://nextjs.org">{time}</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
